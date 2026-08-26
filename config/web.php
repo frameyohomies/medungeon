5f4174c4ff363d4f6fc4a7efce11c958d0ad61e4
Clemens Koechl <clemens3580@gmail.com>
2026-08-26 10:36:41 +0200

    SSO works fine

--- config/web.php
+++ config/web.php
@@ -61,11 +61,11 @@ $config = [
             'clients' => [
                 'azure' => [
                     'class' => 'chunlaw\authclient\Azure',
-                    'authUrl' => 'https://login.microsoftonline.com/44f224f4-9869-4112-ac48-f18eb4419850/oauth2/v2.0/authorize',
+                    'authUrl' => 'https://login.microsoftonline.com/44f224f4-9869-4112-ac48-f18eb4419850/oauth2/v2.0/authorize?prompt=select_account',
                     'tokenUrl' => 'https://login.microsoftonline.com/44f224f4-9869-4112-ac48-f18eb4419850/oauth2/v2.0/token',
                     'returnUrl' => 'https://medino-inventur.ddev.site/site/auth?authclient=azure',
                     'clientId' => '54797bd5-6281-42f3-933f-115f5f047e75',
-                    'clientSecret' => Yii::$app->params['azureClientSecret'] ?? '',
+                    'clientSecret' => $params['azureClientSecret'] ?? '',
                 ],
             ],
         ],
